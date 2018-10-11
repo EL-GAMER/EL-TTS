7c0b211cc5b40436940ef79f5d6fd595418e1b17
getek <getekmer@outlook.com>
2018-10-11 22:19:32 +0530

    first stable version

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TTSTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Play = new System.Windows.Forms.Button();
+            this.rateTrackBar = new System.Windows.Forms.TrackBar();
+            this.VoiceList_Combo = new System.Windows.Forms.ComboBox();
+            this.volumeTrackBar = new System.Windows.Forms.TrackBar();
+            this.Voices_Label = new System.Windows.Forms.Label();
+            this.Rate_Label = new System.Windows.Forms.Label();
+            this.Volume_Label = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.rateTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeTrackBar)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(252, 200);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(75, 23);
+            this.Play.TabIndex = 0;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Play_Button_click);
+            // 
+            // rateTrackBar
+            // 
+            this.rateTrackBar.Location = new System.Drawing.Point(103, 86);
+            this.rateTrackBar.Maximum = 6;
+            this.rateTrackBar.Minimum = -6;
+            this.rateTrackBar.Name = "rateTrackBar";
+            this.rateTrackBar.Size = new System.Drawing.Size(224, 45);
+            this.rateTrackBar.TabIndex = 1;
+            this.rateTrackBar.Scroll += new System.EventHandler(this.RateTrackBarHandler);
+            // 
+            // VoiceList_Combo
+            // 
+            this.VoiceList_Combo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.VoiceList_Combo.FormattingEnabled = true;
+            this.VoiceList_Combo.Location = new System.Drawing.Point(103, 35);
+            this.VoiceList_Combo.Name = "VoiceList_Combo";
+            this.VoiceList_Combo.Size = new System.Drawing.Size(224, 21);
+            this.VoiceList_Combo.TabIndex = 2;
+            this.VoiceList_Combo.SelectedIndexChanged += new System.EventHandler(this.Voice_List_changed);
+            // 
+            // volumeTrackBar
+            // 
+            this.volumeTrackBar.Location = new System.Drawing.Point(103, 137);
+            this.volumeTrackBar.Maximum = 60;
+            this.volumeTrackBar.Name = "volumeTrackBar";
+            this.volumeTrackBar.Size = new System.Drawing.Size(224, 45);
+            this.volumeTrackBar.TabIndex = 3;
+            this.volumeTrackBar.TickFrequency = 4;
+            this.volumeTrackBar.Value = 10;
+            this.volumeTrackBar.Scroll += new System.EventHandler(this.Volume_TrackBar);
+            // 
+            // Voices_Label
+            // 
+            this.Voices_Label.AutoSize = true;
+            this.Voices_Label.Location = new System.Drawing.Point(22, 40);
+            this.Voices_Label.Name = "Voices_Label";
+            this.Voices_Label.Size = new System.Drawing.Size(39, 13);
+            this.Voices_Label.TabIndex = 4;
+            this.Voices_Label.Text = "Voices";
+            // 
+            // Rate_Label
+            // 
+            this.Rate_Label.AutoSize = true;
+            this.Rate_Label.Location = new System.Drawing.Point(22, 90);
+            this.Rate_Label.Name = "Rate_Label";
+            this.Rate_Label.Size = new System.Drawing.Size(30, 13);
+            this.Rate_Label.TabIndex = 5;
+            this.Rate_Label.Text = "Rate";
+            // 
+            // Volume_Label
+            // 
+            this.Volume_Label.AutoSize = true;
+            this.Volume_Label.Location = new System.Drawing.Point(22, 142);
+            this.Volume_Label.Name = "Volume_Label";
+            this.Volume_Label.Size = new System.Drawing.Size(42, 13);
+            this.Volume_Label.TabIndex = 6;
+            this.Volume_Label.Text = "Volume";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "EL TTS";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.showToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 274);
+            this.Controls.Add(this.Volume_Label);
+            this.Controls.Add(this.Rate_Label);
+            this.Controls.Add(this.Voices_Label);
+            this.Controls.Add(this.volumeTrackBar);
+            this.Controls.Add(this.VoiceList_Combo);
+            this.Controls.Add(this.rateTrackBar);
+            this.Controls.Add(this.Play);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EL TTS";
+            this.Closed += new System.EventHandler(this.Form1_Closed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rateTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeTrackBar)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.TrackBar rateTrackBar;
+        private System.Windows.Forms.ComboBox VoiceList_Combo;
+        private System.Windows.Forms.TrackBar volumeTrackBar;
+        private System.Windows.Forms.Label Voices_Label;
+        private System.Windows.Forms.Label Rate_Label;
+        private System.Windows.Forms.Label Volume_Label;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Threading;
+using Microsoft.Win32;
+
+
+namespace TTSTest
+{
+    public partial class Form1 : Form
+    {
+        private static bool _speeking=false;
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hwnd, int id, uint fsModifier, uint vk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hwnd, int id);
+
+        private const string RegLoc=@"SOFTWARE\LackTTS";
+        private RegistryKey _key;
+
+        private Config _prevConfigFile;
+        
+        private static SpeechSynthesizer _synth;
+
+        public Form1() => InitializeComponent();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = Properties.Resources.pixel;
+            InitializeComp();
+
+
+            
+            RegisterHotKey(Handle, 13, (uint) FsModifier.Control, (uint) Keys.E);
+        }
+
+        private void Form1_Closed(object sender, EventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 13);
+            if (!IsConfigChange()) return;
+            RemoveConfigKey();
+            AddConfigKey();
+        }
+        private enum FsModifier
+        {
+            Alt=0x0002,
+            Control=0x0002,
+            Shift=0x0004,
+            Windows=0x0008
+        }
+
+        
+        private string GetMainText()
+        {
+            if (Focused)
+                return Clipboard.ContainsText(TextDataFormat.Text) ? Clipboard.GetText(TextDataFormat.Text) : null;
+            try
+            {
+                SendKeys.SendWait("^c");
+            }
+            catch
+            {
+                throw new InvalidOperationException("not selected any application");
+            }
+
+            return Clipboard.ContainsText(TextDataFormat.Text) ? Clipboard.GetText(TextDataFormat.Text) : null;
+        }
+
+        #region Configuration file
+
+        private bool IsConfigChange()
+        {
+            if (_prevConfigFile == null) return false;
+            var current = GetCurrentConfig();
+            return !(_prevConfigFile.Voice == current.Voice & _prevConfigFile.Rate == current.Rate &
+                     _prevConfigFile.Volume == current.Volume);
+        }
+
+        private void AddConfigKey()
+        {
+            _key = Registry.CurrentUser.OpenSubKey(RegLoc,true);
+
+            
+            using (var ms = new MemoryStream())
+            {
+                try
+                {
+
+                    var formatter = new BinaryFormatter();
+                    formatter.Serialize(ms, GetCurrentConfig());
+                    Console.WriteLine("come on");
+                    var data = ms.ToArray();
+                    _key?.SetValue("config", data, RegistryValueKind.Binary);
+
+                }catch(System.Runtime.Serialization.SerializationException e)
+                {
+                    Console.WriteLine("fuckkkkadfaslfasd");
+                    Console.WriteLine(e.Message);
+                }catch(System.UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("problem in adding config key");
+                }
+                
+            }
+
+            _key?.Close();
+        }
+        
+        private void RemoveConfigKey()
+        {
+            _key = Registry.CurrentUser.OpenSubKey(RegLoc, true);
+            if (_key == null) return;
+            try
+            {
+                _key.DeleteValue("LackTTS");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        private void InitConfig(Config config)
+        {
+            try
+            {
+                VoiceList_Combo.SelectedIndex = config.Voice;
+            }catch(NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            rateTrackBar.Value = config.Rate;
+            volumeTrackBar.Value = config.Volume;
+        }
+        
+        private Config GetConfigFromReg()
+        {
+            _key = Registry.CurrentUser.OpenSubKey(RegLoc);
+            if (_key == null) return new Config();
+            try
+            {
+                using (var ms = new MemoryStream((byte[]) _key.GetValue("config")))
+                {
+                    var formatter = new BinaryFormatter();
+                    return (Config) formatter.Deserialize(ms);
+
+                }
+
+            }
+            catch (System.Runtime.Serialization.SerializationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (System.ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return new Config();
+        }
+
+        private Config GetCurrentConfig() => new Config{Rate = rateTrackBar.Value,Voice = VoiceList_Combo.SelectedIndex,Volume = volumeTrackBar.Value};
+
+        private void CreateConfig()
+        {
+            CreateConfig(0,20,0);
+        }
+        
+        private void CreateConfig(int rate, int volume, int voice)
+        {
+            _key = Registry.CurrentUser.CreateSubKey(RegLoc);
+            _prevConfigFile = new Config{Rate = rate,Voice = voice,Volume = volume};
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                try
+                {
+                    formatter.Serialize(ms, _prevConfigFile);
+                }catch(System.Runtime.Serialization.SerializationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                var data = ms.ToArray();
+                _key?.SetValue("config", data, RegistryValueKind.Binary);
+            }
+
+            _key?.Close();
+        }
+
+        private bool IsExistConfig()
+        {
+            using (_key = Registry.CurrentUser.OpenSubKey(RegLoc))
+                return _key != null;
+            Console.WriteLine("yes");
+        }
+        
+        #endregion
+        
+        
+        #region Initializing sample
+
+        private void InitializeComp()
+        {
+            _synth = new SpeechSynthesizer();
+            foreach (var voice in _synth.GetInstalledVoices())
+            {
+                VoiceList_Combo.Items.Add(voice.VoiceInfo.Name);
+            }
+
+            if (IsExistConfig())
+            {
+                _prevConfigFile = GetConfigFromReg();
+                InitConfig(_prevConfigFile);
+            }
+            else
+            {
+                CreateConfig();
+                VoiceList_Combo.SelectedIndex = 0;
+            }
+        }
+
+        private void TextToSpeech()
+        {
+            
+            if (!_speeking)
+            {
+                var stringToPlay = GetMainText();
+                if (stringToPlay == null) return;
+                var t1 = new Thread(() =>
+                {
+                    _speeking = true;
+                    try
+                    {
+                        _synth.Speak(stringToPlay);
+                    }
+                    catch(OperationCanceledException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    
+                    _speeking = false;
+                }) {IsBackground = true};
+                t1.SetApartmentState(ApartmentState.STA);
+                t1.Start();
+            }
+            else
+            {
+                
+                _synth.Dispose();
+                _synth=new SpeechSynthesizer();
+                _synth.SelectVoice(VoiceList_Combo.SelectedItem.ToString());
+                _speeking = false;
+            }
+        }
+        #endregion
+
+        #region Minimize to tray code
+
+        private void OnMinimize()
+        {
+            notifyIcon1.BalloonTipText="Select the text and press ctrl+E to speak";
+
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(2000);
+            this.Hide();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // Trap WM_SYSCOMMAND, SC_MINIMIZE
+            if (m.Msg == 0x112 && m.WParam.ToInt32() == 0xf020)
+            {
+                OnMinimize();
+                return; // NOTE: delete if you still want the default behavior
+            }
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312 & m.WParam.ToInt32() == 13)
+            {
+                TextToSpeech();
+            }
+            ;
+        }
+
+        #endregion
+
+        #region notity icon code
+
+        //on double click the notify icon
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        //on notify icon exit menu
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //on notify icon show menu
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        #endregion
+
+        #region Handlers
+
+        //Rate handler
+        private void RateTrackBarHandler(object sender, EventArgs e)
+        {
+            if (rateTrackBar.Value >= -5 & rateTrackBar.Value <= 5)
+                _synth.Rate = rateTrackBar.Value;
+        }
+
+        //volume handler
+        private void Volume_TrackBar(object sender, EventArgs e)
+        {
+            if (volumeTrackBar.Value >= 0 & volumeTrackBar.Value <= 100)
+            {
+                _synth.Volume = volumeTrackBar.Value;
+            }
+        }
+
+        //play button handler
+        private void Play_Button_click(object sender, MouseEventArgs e)
+        {
+            TextToSpeech();
+        }
+
+        //voice list combo box handler
+        private void Voice_List_changed(object sender, EventArgs e)
+        {
+            
+            
+                if(!VoiceList_Combo.SelectedItem.Equals(null))
+                    _synth.SelectVoice(VoiceList_Combo.SelectedItem.ToString());
+            
+        }
+
+        #endregion
+    }
+    
+    [Serializable]
+    public class Config
+    {
+        public int Voice { get; set; } = 0;
+        public int Volume { get; set; } = 20;
+        public int Rate { get; set; } = 0;
+    }
+}
